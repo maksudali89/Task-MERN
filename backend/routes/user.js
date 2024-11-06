218340e3d55137739fd5a749c03912d3110bb259
Ali-maksud <alimaksud9389@gmail.com>
2024-11-06 17:30:27 +0530

    add

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,12 +5,13 @@ import {HandlerForUserSignUp,HandlerForUserLogin} from '../controllers/user.js';
 import {AuthMiddleware} from '../middleware/auth.js'
 router.post('/signup',HandlerForUserSignUp);
 router.post('/login',HandlerForUserLogin);
-router.get('/test',AuthMiddleware,async(req,res)=>{
+router.get('/test',async(req,res)=>{
     // console.log(req.header);
     // const {name} = await req.body;
-    const user =  req.user;
-    console.log(user)
-    return res.status(200).json({message:"test Successfully ",User:user})
+    // const user =  req.user;
+    // console.log(user)
+    return res.end("testing purpose")
+    // return res.status(200).json({message:"test Successfully ",User:user})
 })
 
 export default router;
